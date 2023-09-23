335e6ff076e86b5fb2d9dba491c67c667da56859
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-09-22 21:41:34 -0500

    Update docking_pipline.py

--- docking_pipline.py
+++ docking_pipline.py
@@ -54,12 +54,9 @@ class PocketPrediction:
         try:
             if not os.path.exists(os.path.join(self.outpath_vina, outfile_name)):
                 # Run the command and wait for it to complete
-                completed_process = subprocess.run([f"./runVina.sh -l {} -r {} -o {} -center {} -size {}"], check=True, capture_output=True, text=True)
+                completed_process = subprocess.run([f"./runVina.sh -l {self.ligand_path} -r {self.protein_path} -o {self.outpath_vina} --center {} --size {}"], check=True, capture_output=True, text=True)
                 print(f"Return code: {completed_process.returncode}") #an exit status of 0 indicates that it ran successfully
                 print(f"Output: {completed_process.stdout}")
-                # Move the output file to the desired location
-                shutil.move(os.path.join(protein_path, outfile_name), 
-                            os.path.join(outpath_fpocket))
 
         except subprocess.CalledProcessError as e:
             print(f"An error occurred: {e}")
