433c211c055ac6b0ef6004b4439116a56ba511a3
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-19 22:06:33 -0500

    Update drug_pipeline.py

--- drug_pipeline.py
+++ drug_pipeline.py
@@ -179,7 +179,7 @@ class PocketPrediction:
             pdb_name = basename.split("_")[0] #Cuz we are reading with _beta.pdb suffix
             
             # size =  --center {ast.literal_eval(center)} --size {ast.literal_eval(size)}
-            completed_process = subprocess.run([f"{self.vina_script_path} -l {self.ligand_path} -r {pdb_file} --center {center)} --size {size} -o {self.outpath_vina}"], shell=True, check=True, capture_output=True, text=True)
+            completed_process = subprocess.run([f"{self.vina_script_path} -l {self.ligand_path} -r {pdb_file} --center {center} --size {size} -o {self.outpath_vina}"], shell=True, check=True, capture_output=True, text=True)
             print(f"Return code: {completed_process.returncode}") #an exit status of 0 indicates that it ran successfully
             print(f"Output: {completed_process.stdout}")
             print(f"Output: {completed_process.stderr}")
