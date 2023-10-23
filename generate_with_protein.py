32d251279fb280339c8f3da22200a58b77d16488
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-22 22:35:19 -0500

    Update generate_with_protein.py

--- generate_with_protein.py
+++ generate_with_protein.py
@@ -207,102 +207,103 @@ def main(input_path, protein_path, backbone_atoms_only, model,
         return
 
     try:
-        molecule = read_molecule(input_path)
-        molecule = Chem.RemoveAllHs(molecule)
+        molecules = read_molecule(input_path)
+        molecules = Chem.RemoveAllHs(molecules)
         name = '.'.join(input_path.split('/')[-1].split('.')[:-1])
     except Exception as e:
         return f'Could not read the file with fragments: {e}'
 
-    # Parsing fragments data
-    frag_pos, frag_one_hot, frag_charges = parse_molecule(molecule, is_geom=ddpm.is_geom)
-
-    # Parsing pocket data
-    try:
-        pocket_pos, pocket_one_hot, pocket_charges = get_pocket(molecule, protein_path, backbone_atoms_only)
-    except Exception as e:
-        return f'Could not read the file with pocket: {e}'
-
-    positions = np.concatenate([frag_pos, pocket_pos], axis=0)
-    one_hot = np.concatenate([frag_one_hot, pocket_one_hot], axis=0)
-    charges = np.concatenate([frag_charges, pocket_charges], axis=0)
-    anchor_flags = np.zeros_like(charges)
-    if anchors is not None:
-        for anchor in anchors.split(','):
-            anchor_flags[int(anchor.strip()) - 1] = 1
-
-    fragment_only_mask = np.concatenate([
-        np.ones_like(frag_charges),
-        np.zeros_like(pocket_charges),
-    ])
-    pocket_mask = np.concatenate([
-        np.zeros_like(frag_charges),
-        np.ones_like(pocket_charges),
-    ])
-    linker_mask = np.concatenate([
-        np.zeros_like(frag_charges),
-        np.zeros_like(pocket_charges),
-    ])
-    fragment_mask = np.concatenate([
-        np.ones_like(frag_charges),
-        np.ones_like(pocket_charges),
-    ])
-
-    dataset = [{
-        'uuid': '0',
-        'name': '0',
-        'positions': torch.tensor(positions, dtype=const.TORCH_FLOAT, device=device),
-        'one_hot': torch.tensor(one_hot, dtype=const.TORCH_FLOAT, device=device),
-        'charges': torch.tensor(charges, dtype=const.TORCH_FLOAT, device=device),
-        'anchors': torch.tensor(anchor_flags, dtype=const.TORCH_FLOAT, device=device),
-        'fragment_only_mask': torch.tensor(fragment_only_mask, dtype=const.TORCH_FLOAT, device=device),
-        'pocket_mask': torch.tensor(pocket_mask, dtype=const.TORCH_FLOAT, device=device),
-        'fragment_mask': torch.tensor(fragment_mask, dtype=const.TORCH_FLOAT, device=device),
-        'linker_mask': torch.tensor(linker_mask, dtype=const.TORCH_FLOAT, device=device),
-        'num_atoms': len(positions),
-    }] * n_samples
-    dataset = MOADDataset(data=dataset)
-    ddpm.val_dataset = dataset
-
-    global_batch_size = min(n_samples, max_batch_size)
-    dataloader = get_dataloader(
-        dataset, batch_size=global_batch_size, collate_fn=collate_with_fragment_without_pocket_edges
-    )
-
-    # Sampling
-    print('Sampling...')
-    for batch_i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
-        batch_size = len(data['positions'])
-
-        chain = None
-        for i in range(5):
-            try:
-                chain, node_mask = ddpm.sample_chain(data, sample_fn=sample_fn, keep_frames=1)
-                break
-            except FoundNaNException:
-                continue
-        if chain is None:
-            raise Exception('Could not generate in 5 attempts')
-
-        x = chain[0][:, :, :ddpm.n_dims]
-        h = chain[0][:, :, ddpm.n_dims:]
-
-        # Put the molecule back to the initial orientation
-        com_mask = data['fragment_only_mask'] if ddpm.center_of_mass == 'fragments' else data['anchors']
-        pos_masked = data['positions'] * com_mask
-        N = com_mask.sum(1, keepdims=True)
-        mean = torch.sum(pos_masked, dim=1, keepdim=True) / N
-        x = x + mean * node_mask
-
-        offset_idx = batch_i * global_batch_size
-        names = [f'output_{offset_idx+i}_{name}' for i in range(batch_size)]
-
-        node_mask[torch.where(data['pocket_mask'])] = 0
-        save_xyz_file(output_dir, h, x, node_mask, names=names, is_geom=ddpm.is_geom, suffix='')
-
-        for i in range(batch_size):
-            out_xyz = f'{output_dir}/output_{offset_idx+i}_{name}_.xyz'
-            out_sdf = f'{output_dir}/output_{offset_idx+i}_{name}_.sdf'
-            subprocess.run(f'obabel {out_xyz} -O {out_sdf} 2> /dev/null', shell=True)
+    for nth_molecule, molecule in enumerate(molecules):             
+        # Parsing fragments data
+        frag_pos, frag_one_hot, frag_charges = parse_molecule(molecule, is_geom=ddpm.is_geom)
+    
+        # Parsing pocket data
+        try:
+            pocket_pos, pocket_one_hot, pocket_charges = get_pocket(molecule, protein_path, backbone_atoms_only)
+        except Exception as e:
+            return f'Could not read the file with pocket: {e}'
+    
+        positions = np.concatenate([frag_pos, pocket_pos], axis=0)
+        one_hot = np.concatenate([frag_one_hot, pocket_one_hot], axis=0)
+        charges = np.concatenate([frag_charges, pocket_charges], axis=0)
+        anchor_flags = np.zeros_like(charges)
+        if anchors is not None:
+            for anchor in anchors.split(','):
+                anchor_flags[int(anchor.strip()) - 1] = 1
+    
+        fragment_only_mask = np.concatenate([
+            np.ones_like(frag_charges),
+            np.zeros_like(pocket_charges),
+        ])
+        pocket_mask = np.concatenate([
+            np.zeros_like(frag_charges),
+            np.ones_like(pocket_charges),
+        ])
+        linker_mask = np.concatenate([
+            np.zeros_like(frag_charges),
+            np.zeros_like(pocket_charges),
+        ])
+        fragment_mask = np.concatenate([
+            np.ones_like(frag_charges),
+            np.ones_like(pocket_charges),
+        ])
+    
+        dataset = [{
+            'uuid': '0',
+            'name': '0',
+            'positions': torch.tensor(positions, dtype=const.TORCH_FLOAT, device=device),
+            'one_hot': torch.tensor(one_hot, dtype=const.TORCH_FLOAT, device=device),
+            'charges': torch.tensor(charges, dtype=const.TORCH_FLOAT, device=device),
+            'anchors': torch.tensor(anchor_flags, dtype=const.TORCH_FLOAT, device=device),
+            'fragment_only_mask': torch.tensor(fragment_only_mask, dtype=const.TORCH_FLOAT, device=device),
+            'pocket_mask': torch.tensor(pocket_mask, dtype=const.TORCH_FLOAT, device=device),
+            'fragment_mask': torch.tensor(fragment_mask, dtype=const.TORCH_FLOAT, device=device),
+            'linker_mask': torch.tensor(linker_mask, dtype=const.TORCH_FLOAT, device=device),
+            'num_atoms': len(positions),
+        }] * n_samples
+        dataset = MOADDataset(data=dataset)
+        ddpm.val_dataset = dataset
+    
+        global_batch_size = min(n_samples, max_batch_size)
+        dataloader = get_dataloader(
+            dataset, batch_size=global_batch_size, collate_fn=collate_with_fragment_without_pocket_edges
+        )
+    
+        # Sampling
+        print('Sampling...')
+        for batch_i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+            batch_size = len(data['positions'])
+    
+            chain = None
+            for i in range(5):
+                try:
+                    chain, node_mask = ddpm.sample_chain(data, sample_fn=sample_fn, keep_frames=1)
+                    break
+                except FoundNaNException:
+                    continue
+            if chain is None:
+                raise Exception('Could not generate in 5 attempts')
+    
+            x = chain[0][:, :, :ddpm.n_dims]
+            h = chain[0][:, :, ddpm.n_dims:]
+    
+            # Put the molecule back to the initial orientation
+            com_mask = data['fragment_only_mask'] if ddpm.center_of_mass == 'fragments' else data['anchors']
+            pos_masked = data['positions'] * com_mask
+            N = com_mask.sum(1, keepdims=True)
+            mean = torch.sum(pos_masked, dim=1, keepdim=True) / N
+            x = x + mean * node_mask
+    
+            offset_idx = batch_i * global_batch_size
+            names = [f'output_{nth_molecule}_{offset_idx+i}_{name}' for i in range(batch_size)]
+    
+            node_mask[torch.where(data['pocket_mask'])] = 0
+            save_xyz_file(output_dir, h, x, node_mask, names=names, is_geom=ddpm.is_geom, suffix='')
+    
+            # for i in range(batch_size):
+            #     out_xyz = f'{output_dir}/output_{offset_idx+i}_{name}_.xyz'
+            #     out_sdf = f'{output_dir}/output_{offset_idx+i}_{name}_.sdf'
+            #     subprocess.run(f'obabel {out_xyz} -O {out_sdf} 2> /dev/null', shell=True)
 
     print(f'Saved generated molecules in .xyz and .sdf format in directory {output_dir}')
 
