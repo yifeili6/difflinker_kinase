64a8b1bd451d2bef334f2fcba1eac244fd5670ce
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-02 00:55:48 -0600

    Update posebuster_analysis.py

--- posebuster_analysis.py
+++ posebuster_analysis.py
@@ -14,7 +14,7 @@ def get_posebuster_stats(filename: str):
     print(df)
 
 if __name__ == "__main__":
-    get_posebuster_states(args.filename)
+    get_posebuster_stats(args.filename)
 
     ## Current as of [Feb 1st 2024]
     ## git pull && python -m posebuster_analysis --filename data_docking/result_difflinker/output_0_2_KLIF_test_frag_.xyz
