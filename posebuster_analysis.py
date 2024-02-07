0a0930007eaa9b79532681d7016fc64486ac6214
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-06 22:58:41 -0600

    Update posebuster_analysis.py

--- posebuster_analysis.py
+++ posebuster_analysis.py
@@ -2,20 +2,21 @@ from posebusters import PoseBusters
 from pathlib import Path
 from rdkit import Chem
 import argparse
+from typing import List
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--filename", "-f", type=str, help="xyz file name")
+parser.add_argument("--filenames", "-f", nargs="*", help="xyz file names")
 args = parser.parse_args()
 
-def get_posebuster_stats(filename: str):
-    pred_file = Chem.MolFromXYZFile(filename)
+def get_posebuster_stats(filenames: List[str]):
+    pred_files = [Chem.MolFromXYZFile(os.path.join("data_docking/result_difflinker", filename)) for filename in filenames] 
     buster = PoseBusters(config="mol")
-    df = buster.bust([pred_file], None, None, full_report=False)
+    df = buster.bust(pred_files, None, None, full_report=False)
     print(df.columns)
     print(df.values)
 
 if __name__ == "__main__":
-    get_posebuster_stats(args.filename)
+    get_posebuster_stats(args.filenames)
 
     ## Current as of [Feb 1st 2024]
-    ## git pull && python -m posebuster_analysis --filename data_docking/result_difflinker/output_0_2_KLIF_test_frag_.xyz
+    ## git pull && python -m posebuster_analysis --filenames output_0_2_KLIF_test_frag_.xyz output_0_3_KLIF_test_frag_.xyz
