48c72ee56a3b1f9452cc15a6bb47a180b65fa4a5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-09 23:19:10 -0600

    Update misc_utils.py

--- misc_utils.py
+++ misc_utils.py
@@ -15,7 +15,7 @@ def get_kinase_indices(kinase_names: List[str]) -> List[List[str]]:
     df=pd.concat([df0, df1, df2], axis=0)
     df.reset_index(drop=True, inplace=True)
     
-    indices = df.index[df.molecule_name.apply(lambda inp: inp in kinase_names)].tolist()
+    indices = df.index[df.molecule_name.apply(lambda inp: inp == '2xa4')].tolist()
 
     return indices
 
