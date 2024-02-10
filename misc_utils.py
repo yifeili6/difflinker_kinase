a2a1c7fc8f3643c4ef6d627ace2fb292b1af0c62
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-10 17:20:09 -0600

    Update misc_utils.py

--- misc_utils.py
+++ misc_utils.py
@@ -12,7 +12,7 @@ def get_kinase_indices(kinase_names: List[str]) -> List[List[str]]:
     df1=pd.read_csv("/Scr/yifei6/difflinker_kinase/data_docking/complex/processed_klif_wl/KLIF_val_table.csv", header=0)
     df2=pd.read_csv("/Scr/yifei6/difflinker_kinase/data_docking/complex/processed_klif_wl/KLIF_test_table.csv", header=0)
     
-    df=pd.concat([df0, df1, df2], axis=0)
+    df=pd.concat([df1, df2], axis=0)
     df.reset_index(drop=True, inplace=True)
     
     indices = df.index[df.molecule_name.apply(lambda inp: inp[:4] in kinase_names)].tolist()
