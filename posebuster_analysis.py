924efe96f497e4151561226568f380767268da73
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-10 18:38:52 -0600

    Update posebuster_analysis.py

--- posebuster_analysis.py
+++ posebuster_analysis.py
@@ -10,7 +10,7 @@ parser.add_argument("--filenames", "-f", nargs="*", help="xyz file names")
 args = parser.parse_args()
 
 def get_posebuster_stats(filenames: List[str]):
-    pred_files = [Chem.MolFromXYZFile(os.path.join("data_docking/result_difflinker", filename)) for filename in filenames] 
+    pred_files = [Chem.MolFromXYZFile(os.path.join("data_docking/result_difflinker", filename)) if filename.endswith("xyz") else Chem.SDMolSupplier(os.path.join("data_docking/result_difflinker", filename))[0] for filename in filenames] 
     buster = PoseBusters(config="mol")
     df = buster.bust(pred_files, None, None, full_report=False)
     print(df.columns)
