e29a5f58922fa46bcedfce4fc287d53f976353d0
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-10 18:43:52 -0600

    Update posebuster_analysis.py

--- posebuster_analysis.py
+++ posebuster_analysis.py
@@ -15,6 +15,7 @@ def get_posebuster_stats(filenames: List[str]):
     df = buster.bust(pred_files, None, None, full_report=False)
     # print(df.columns)
     # print(df.values)
+    df.drop(index="molecule", inplace=True)
     print(df)
 
 if __name__ == "__main__":
