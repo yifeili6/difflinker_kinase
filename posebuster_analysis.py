6428661c9f6aa006284164001ef4d0d35f6b9ac8
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-11 21:26:16 -0600

    Update posebuster_analysis.py

--- posebuster_analysis.py
+++ posebuster_analysis.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 
 def get_posebuster_stats(kinase_prefix_names: List[str]):
     for kinase_file_prefix in kinase_prefix_names:
-        filenames = sorted(glob.glob((os.path.join("data_docking/result_difflinker", kinase_file_prefix + "*")))
+        filenames = sorted(glob.glob(os.path.join("data_docking/result_difflinker", kinase_file_prefix + "*")))
         # pred_files = [Chem.MolFromXYZFile(os.path.join("data_docking/result_difflinker", filename)) if filename.endswith("xyz") else Chem.SDMolSupplier(os.path.join("data_docking/result_difflinker", filename))[0] for filename in filenames] 
         pred_files = [Chem.SDMolSupplier(filename)[0] for filename in filenames] 
         buster = PoseBusters(config="mol")
