96f1173de5e80fd9bb5c5a7a5fb3f6cbc40027f1
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-13 00:13:05 -0600

    Update misc_utils.py

--- misc_utils.py
+++ misc_utils.py
@@ -8,9 +8,9 @@ parser.add_argument('--kinase_names', nargs="*", type=str, required=True)
 def get_kinase_indices(kinase_names: List[str]) -> List[List[str]]:
     """Return corresponding indices of kinase indices"""
   
-    df0=pd.read_csv("/Scr/yifei6/difflinker_kinase/data_docking/complex/processed_klif_wl/KLIF_train_table.csv", header=0)
-    df1=pd.read_csv("/Scr/yifei6/difflinker_kinase/data_docking/complex/processed_klif_wl/KLIF_val_table.csv", header=0)
-    df2=pd.read_csv("/Scr/yifei6/difflinker_kinase/data_docking/complex/processed_klif_wl/KLIF_test_table.csv", header=0)
+    df0=pd.read_csv("datasets/KLIF_train_table.csv", header=0)
+    df1=pd.read_csv("datasets/KLIF_val_table.csv", header=0)
+    df2=pd.read_csv("datasets/KLIF_test_table.csv", header=0)
     
     df=pd.concat([df1, df2], axis=0)
     df.reset_index(drop=True, inplace=True)
