83ec185a9d3f534f1bc77569e6a921680137bdd8
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-13 16:28:01 -0600

    Update gen_lig_analysis.py

--- gen_lig_analysis.py
+++ gen_lig_analysis.py
@@ -11,6 +11,9 @@ import warnings
 from rdkit import rdBase
 import moses
 import numpy as np
+from rdkit.Chem import Crippen
+from rdkit.Chem import Lipinski
+from rdkit.Chem import Descriptors
 
 rdBase.DisableLog('rdApp.*')
 
@@ -78,6 +81,89 @@ def get_moses_stats(gen, k=None, n_jobs=os.cpu_count()-1,
     print(cf.on_blue("MOSES metrics"))    
     print(metrics)
     return metrics
+
+def get_lipinski(gen: List[str]):
+    """
+    Source:
+    https://gist.github.com/strets123/fdc4db6d450b66345f46
+    """
+    class SmilesError(Exception): pass
+    
+    def log_partition_coefficient(smiles):
+        '''
+        Returns the octanol-water partition coefficient given a molecule SMILES 
+        string
+        '''
+        try:
+            mol = Chem.MolFromSmiles(smiles)
+        except Exception, e:
+            raise SmilesError('%s returns a None molecule' % smiles)
+            
+        return Crippen.MolLogP(mol)
+        
+    def lipinski_trial(smiles):
+        '''
+        Returns which of Lipinski's rules a molecule has failed, or an empty list
+        
+        Lipinski's rules are:
+        Hydrogen bond donors <= 5
+        Hydrogen bond acceptors <= 10
+        Molecular weight < 500 daltons
+        logP < 5
+        '''
+        passed = []
+        failed = []
+        
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            raise Exception('%s is not a valid SMILES string' % smiles)
+        
+        num_hdonors = Lipinski.NumHDonors(mol)
+        num_hacceptors = Lipinski.NumHAcceptors(mol)
+        mol_weight = Descriptors.MolWt(mol)
+        mol_logp = Crippen.MolLogP(mol)
+        
+        failed = []
+        
+        if num_hdonors > 5:
+            failed.append('Over 5 H-bond donors, found %s' % num_hdonors)
+        else:
+            passed.append('Found %s H-bond donors' % num_hdonors)
+            
+        if num_hacceptors > 10:
+            failed.append('Over 10 H-bond acceptors, found %s' \
+            % num_hacceptors)
+        else:
+            passed.append('Found %s H-bond acceptors' % num_hacceptors)
+            
+        if mol_weight >= 500:
+            failed.append('Molecular weight over 500, calculated %s'\
+            % mol_weight)
+        else:
+            passed.append('Molecular weight: %s' % mol_weight)
+            
+        if mol_logp >= 5:
+            failed.append('Log partition coefficient over 5, calculated %s' \
+            % mol_logp)
+        else:
+            passed.append('Log partition coefficient: %s' % mol_logp)
+        
+        return passed, failed
+        
+    def lipinski_pass(smiles):
+        '''
+        Wraps around lipinski trial, but returns a simple pass/fail True/False
+        '''
+        passed, failed = lipinski_trial(smiles)
+        if failed:
+            return False
+        else:
+            return True
+
+    gen: List[str] = [Chem.MolToSmiles(g) for g in gen if g is not None]
+    lipinski_results = [lipinski_pass(smiles) for smiles in gen]
+    print(np.mean(lipinski_results))
+    return lipinski_results
     
 if __name__ == "__main__":
     gen = get_posebuster_stats(args.kinase_prefix_names)
@@ -86,6 +172,7 @@ if __name__ == "__main__":
     else:
         gen = args.gen
     get_moses_stats(gen, train=args.train, test=args.valtest, test_scaffolds=args.valtest)
+    get_lipinski(gen)
 
     ## Current as of [Feb 1st 2024]
     ## git pull && python -m posebuster_analysis --filenames output_0_2_KLIF_test_frag_.xyz output_0_3_KLIF_test_frag_.xyz
