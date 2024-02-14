7ff29e9e09dbb7990ea06700c1dbdb2604902196
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-14 17:06:34 -0600

    Update gen_lig_analysis.py

--- gen_lig_analysis.py
+++ gen_lig_analysis.py
@@ -14,6 +14,7 @@ import numpy as np
 from rdkit.Chem import Crippen
 from rdkit.Chem import Lipinski
 from rdkit.Chem import Descriptors
+from moses.metrics.utils import mapper, mol_passes_filters
 
 rdBase.DisableLog('rdApp.*')
 
@@ -28,14 +29,18 @@ args = parser.parse_args()
 warnings.simplefilter('ignore')
 
 def get_posebuster_stats(kinase_prefix_names: List[str]):
-    return_good_smiles = []
-    for kinase_file_prefix in kinase_prefix_names:
+    return_good_mols = []
+    return_good_files = []
+
+    for kinase_file_prefix in enumeratekinase_prefix_names:
         try:
             filenames = sorted(glob.glob(os.path.join("data_docking/result_difflinker", kinase_file_prefix + "*")))
             # pred_files = [Chem.MolFromXYZFile(os.path.join("data_docking/result_difflinker", filename)) if filename.endswith("xyz") else Chem.SDMolSupplier(os.path.join("data_docking/result_difflinker", filename))[0] for filename in filenames] 
-            pred_files = [Chem.SDMolSupplier(filename)[0] for filename in filenames] 
-            print(cf.red(f"None location: {np.where(np.array(pred_files) == None)[0]}"))
-            pred_files = [p for p in pred_files if p is not None]
+            pred_files_ = [(filename, Chem.SDMolSupplier(filename)[0]) for filename in filenames] 
+            print(cf.red(f"None location: {np.where(np.array(pred_files)[:, 1] == None)[0]}"))
+            pred_files = [p[1] for p in pred_files_ if p[1] is not None] #List[MOL]
+            pred_file_names = [p[0] for p in pred_files_ if p[1] is not None] #List[str]
+
             buster = PoseBusters(config="mol")
             df = buster.bust(pred_files, None, None, full_report=False)
             # print(df.columns)
@@ -45,8 +50,10 @@ def get_posebuster_stats(kinase_prefix_names: List[str]):
             Df["Total Pass"] = Df.all(axis=1)
             print("Result:\n", Df)
 
-            good_smiles = np.array(pred_files)[Df.values[:,-1].astype(bool)]
-            return_good_smiles.extend(good_smiles.tolist())
+            good_mols = np.array(pred_files)[Df.values[:,-1].astype(bool)]
+            good_mol_files = np.array(pred_file_names)[Df.values[:,-1].astype(bool)]
+            return_good_mols.extend(good_mols.tolist())
+            return_good_files.extend(good_mol_files.tolist())
             # df.drop(index=["molecule", "file"], inplace=True)
             # print(df)
         except Exception as e:
@@ -54,13 +61,16 @@ def get_posebuster_stats(kinase_prefix_names: List[str]):
             print(cf.on_red(f"Something happend... skipping!!!\n {e}"))
             continue
     # print(return_good_smiles)
-    return return_good_smiles
+    return return_good_mols, return_good_files
 
 def get_moses_stats(gen, k=None, n_jobs=os.cpu_count()-1,
                     device='cuda', batch_size=512, pool=None,
                     test=None, test_scaffolds=None,
                     ptest=None, ptest_scaffolds=None,
-                    train=None):
+                    train=None, files=None):
+                        
+    assert len(gen) == len(files), "gen and files must match in length!" ##files variable is not actively used here!
+                        
     if isinstance(gen, list):
         gen: List[str] = [Chem.MolToSmiles(g) for g in gen if g is not None]
     else:
@@ -73,16 +83,21 @@ def get_moses_stats(gen, k=None, n_jobs=os.cpu_count()-1,
     test = pd.read_csv(test).molecule.drop_duplicates().tolist()
     test_scaffolds = pd.read_csv(test_scaffolds).molecule.drop_duplicates().tolist()
 
+    passes = mapper(n_jobs)(mol_passes_filters, gen)
+    gen = np.array(gen)[np.array(passes).astype(bool)]
+    files = np.array(files)[np.array(passes).astype(bool)]
+
     metrics = moses.get_all_metrics(gen=gen, k=k, n_jobs=n_jobs,
                     device=device, batch_size=batch_size, pool=pool,
                     test=test, test_scaffolds=test_scaffolds,
                     ptest=ptest, ptest_scaffolds=ptest_scaffolds,
                     train=train)
+    
     print(cf.on_blue("MOSES metrics"))    
     print(metrics)
-    return metrics
+    return gen, files
 
-def get_lipinski(gen: List[str]):
+def get_lipinski(gen: List[str]), files: List[str]:
     """
     Source:
     https://gist.github.com/strets123/fdc4db6d450b66345f46
@@ -167,27 +182,30 @@ def get_lipinski(gen: List[str]):
         # print(gen)
         gen: List[str] = [Chem.SDMolSupplier(g)[0] for g in gen]
         gen: List[str] = [Chem.MolToSmiles(g) for g in gen if g is not None]
-        
+
+    assert len(gen) == len(files), "gen and files must match in length!"
+
     lipinski_results = [lipinski_pass(smiles) for smiles in gen]
     
     print(cf.on_yellow("Lipinski Rule of 5"))
     print(np.mean(lipinski_results))
 
     return_good_smiles = np.array(gen)[np.array(lipinski_results).astype(bool)]
-    return return_good_smiles
+    return_good_files = np.array(files)[np.array(lipinski_results).astype(bool)]
+    return return_good_smiles, return_good_files
     
 if __name__ == "__main__":
     ###3D
-    gen = get_posebuster_stats(args.kinase_prefix_names) # filtration 1
+    gen, files = get_posebuster_stats(args.kinase_prefix_names) # filtration 1
     if len(gen) !=0:
         pass
     else:
         gen = args.gen
     ###Drugness
-    gen = get_lipinski(gen) #filtration 2
+    gen, files = get_lipinski(gen, files) #filtration 2
     ###2D
-    get_moses_stats(gen, train=args.train, test=args.valtest, test_scaffolds=args.valtest) # final filtration
-
+    gen, files = get_moses_stats(gen, files=files, train=args.train, test=args.valtest, test_scaffolds=args.valtest) # final filtration
+    print(files)
 
     ## Current as of [Feb 1st 2024]
     ## git pull && python -m posebuster_analysis --filenames output_0_2_KLIF_test_frag_.xyz output_0_3_KLIF_test_frag_.xyz
