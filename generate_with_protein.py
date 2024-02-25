38260748779eb270eb7c1d01ef63e8ceb4bbaadc
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-02-24 18:03:18 -0600

    Update generate_with_protein.py

--- generate_with_protein.py
+++ generate_with_protein.py
@@ -71,6 +71,21 @@ parser.add_argument('--timeseries', action="store_true", help='save trajectory?'
 
 parser.add_argument('--kinase_names', nargs="*", type=str, required=False)
 
+def get_kinase_indices(kinase_names: List[str]) -> List[List[str]]:
+    """Return corresponding indices of kinase indices"""
+  
+    df0=pd.read_csv("datasets/KLIF_train_table.csv", header=0)
+    df1=pd.read_csv("datasets/KLIF_val_table.csv", header=0)
+    df2=pd.read_csv("datasets/KLIF_test_table.csv", header=0)
+    
+    df=pd.concat([df1, df2], axis=0)
+    df.reset_index(drop=True, inplace=True)
+    
+    indices = df.index[df.molecule_name.apply(lambda inp: inp[:4] in kinase_names)].tolist()
+    info = df.loc[df.molecule_name.apply(lambda inp: inp[:4] in kinase_names), ["molecule_name", "anchor_1", "anchor_2", "linker_size"]]
+
+    return indices, info
+
 def read_molecule(path):
     if path.endswith('.pdb'):
         return Chem.MolFromPDBFile(path, sanitize=False, removeHs=True)
