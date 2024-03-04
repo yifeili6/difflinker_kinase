ff5159f7ef5c76b2502b4c362dd25dd3d9beb8a7
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-04 01:13:29 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     root_h = "data_docking/result_hydrogenated"
     root_d = "datasets"
     test_ms = [Chem.SDMolSupplier(os.path.join(root_h, f"5lqf_altB_chainA_3_{num}_KLIF_ValTest_frag.sdf"), removeHs=True, sanitize=False)[0] for num in [25, 27, 55, 60, 81, 82] ]
-    test_ms = [edit_ligand(m) for m in test_ms]
+    # test_ms = [edit_ligand(m) for m in test_ms]
     # query = Chem.SDMolSupplier(os.path.join(root, f"5lqf_altB_chainA_3_GT_KLIF_ValTest_frag.sdf"), removeHs=True, sanitize=False)[0]
     qry = Chem.SDMolSupplier(os.path.join(root_d, f"KLIF_test_frag.sdf"), removeHs=True, sanitize=False)[900]
     qry = [Chem.MolFromSmiles(q) for q in Chem.MolToSmiles(qry).split(".")]
