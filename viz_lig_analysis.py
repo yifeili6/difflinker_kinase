07106fa6747e34049e381fd70dbe19dfe205ca6c
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-03 23:56:14 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -1,10 +1,8 @@
 import torch
 import torch.nn.functional as F
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from rdkit.Chem import rdDepictor
-from rdkit.Chem import Draw
-from rdkit.Chem.Draw import rdMolDraw2D
 import matplotlib
 import matplotlib.cm as cm
 from skimage.io import imread
@@ -12,16 +10,76 @@ from cairosvg import svg2png, svg2ps
 import os
 from torch_geometric.data import DataLoader
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit.Chem.Draw import SimilarityMaps
 from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
 import random
 import wandb
 import PIL
 from typing import *
+from rdkit.Chem import rdDepictor
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import SimilarityMaps
+from rdkit.Chem import rdDistGeom
+from rdkit.Chem import rdFMCS
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit import rdBase
+from gen_lig_analysis import Analyse_generation
 
+rdDepictor.SetPreferCoordGen(True)
+IPythonConsole.ipython_3d = True
+
+rdBase.DisableLog('rdApp.*')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--turn_off_run_test", "-to", action="store_false", help="for ValTest_mol.sdf!")
+args = parser.parse_args()
+
+def findMCS(ms: List[Chem.Mol], qry: Chem.Mol):
+    """
+      https://greglandrum.github.io/rdkit-blog/posts/2022-06-23-3d-mcs.html
+      https://greglandrum.github.io/rdkit-blog/posts/2023-10-27-mcswhatsnew.html
+      https://www.blopig.com/blog/2023/06/customising-mcs-mapping-in-rdkit/
+    """
+    # ms: 5lqf_altB_chainA_3_25/27/55/60/81/82_KLIF_ValTest_frag
+  
+    ps = rdFMCS.MCSParameters()
+    ps.AtomCompareParameters.MaxDistance = 1.0
+    ps.AtomTyper = rdFMCS.AtomCompare.CompareAny
+    res = rdFMCS.FindMCS(ms, ps)
+    qry = Chem.MolFromSmarts(res.smartsString)
+    
+    matches = [x.GetSubstructMatch(qry) for x in ms] 
+    
+    conf = Chem.Conformer(qry.GetNumAtoms())
+    for i,mi in enumerate(matches[0]):
+        conf.SetAtomPosition(i,ms2d[0].GetConformer().GetAtomPosition(mi))
+    qry.AddConformer(conf)
+    for m in ms:
+        rdDepictor.GenerateDepictionMatching2DStructure(m, qry)
+      
+    img = Draw.MolsToGridImage(ms, highlightAtomLists=matches, molsPerRow=3, subImgSize=(200,200), legends=[x.GetProp("_Name") for x in ms)    
+    img.save('data_docking/result_images/cdk2_molgrid.png')    
+
+def plot_properties(args: argparse.ArgumentParser):
+    if not args.turn_off_run_test:
+        print(cf.on_blue(f"Concatenating GENERATED data statistics!!!!"))
+        DF, DF_rings_dist = Analyse_generation.collate_fn()
+        print(DF.loc[:, ["IntDiv", "IntDiv2"]])
+        print(DF)
+        print(DF_rings_dist.groupby("size").mean())
+        DF = Analyse_generation.get_non_wass_stats()
+        print(DF)
+    else:
+        print(cf.on_red(f"Concatenating TEST data statistics!!!!"))
+        DF, DF_rings_dist = Analyse_generation.collate_fn_for_test()
+        print(DF.loc[:, ["IntDiv", "IntDiv2"]])
+        print(DF)
+        print(DF_rings_dist.mean(axis=0))
+        DF = Analyse_generation.get_non_wass_stats_for_test()
+        print(DF)
 
 def img_for_mol(mol, atom_weights=[], bond_weights: Union[None, List]=[], start_idx: int=0, edge_index: torch.LongTensor=None, use_custom_draw: bool=True, new_edge_index: torch.LongTensor=None):
   """
@@ -118,49 +176,4 @@ def img_for_mol(mol, atom_weights=[], bond_weights: Union[None, List]=[], start_
 
 if __name__ == "__main__":
     ###Current as of Mar 1st, 2024
-    if args.run_analysis:
-        if not args.turn_off_run_test:
-            print(cf.on_yellow(f"Running Analysis on GENERATED data with {args.size_prefix}!!!!"))
-            ###3D
-            gen, files, file_counter = Analyse_generation.get_posebuster_stats([args.size_prefix]) # filtration 1
-            if len(gen) !=0:
-                pass
-            else:
-                gen = args.gen
-            ###Drugness
-            gen, files = Analyse_generation.get_lipinski(gen, files, file_counter, args.size_prefix) #filtration 2
-            ###2D
-            gen, files = Analyse_generation.get_moses_stats(gen=gen, files=files, train=args.train, test=args.valtest, test_scaffolds=args.valtest, file_counter_from_posebuster=file_counter, size_prefix=args.size_prefix) # final filtration
-            # print(files)
-            # gen = ["c1ccccc1", "c1cnccc1", "C1CCCCC1", "C1CNCCC1", "CCCCCC", "CCNCCC", "c12ccccc1NC=C2", "CC1=C(C=C(C=C1)NC(=O)C2=CC=C(C=C2)CN3CCN(CC3)C)NC4=NC=CC(=N4)C5=CN=CC=C5", "Cc1c(Nc2nccc(-c3cnccc3)n2)cc(NC(=O)c2ccc(CN3CCN(C)CC3)cc2)cc1"]
-            # files = np.arange(len(gen))
-            rot_bonds, num_rings, num_fused_rings, num_hetero_rings, num_aromatic_rings = bonds_and_rings(gen, files, args.size_prefix)
-            # for prop in [rot_bonds, num_rings, num_fused_rings, num_hetero_rings, num_aromatic_rings]:
-            #     print(prop)
-        else:
-            print(cf.on_green(f"Running Analysis on TEST data!!!!"))
-            ###3D
-            gen, file_counter = Analyse_generation.get_posebuster_stats_for_test() # filtration 1
-            ###Drugness
-            gen = Analyse_generation.get_lipinski_for_test(gen, file_counter) #filtration 2
-            ###2D
-            gen = Analyse_generation.get_moses_stats_for_test(gen=gen,  train=args.train, test=args.valtest, test_scaffolds=args.valtest, file_counter_from_posebuster=file_counter) # final filtration
-            rot_bonds, num_rings, num_fused_rings, num_hetero_rings, num_aromatic_rings = Analyse_generation.bonds_and_rings_for_test(gen)
-            
-    else:
-        if not args.turn_off_run_test:
-            print(cf.on_blue(f"Concatenating GENERATED data statistics!!!!"))
-            DF, DF_rings_dist = Analyse_generation.collate_fn()
-            print(DF.loc[:, ["IntDiv", "IntDiv2"]])
-            print(DF)
-            print(DF_rings_dist.groupby("size").mean())
-            DF = Analyse_generation.get_non_wass_stats()
-            print(DF)
-        else:
-            print(cf.on_red(f"Concatenating TEST data statistics!!!!"))
-            DF, DF_rings_dist = Analyse_generation.collate_fn_for_test()
-            print(DF.loc[:, ["IntDiv", "IntDiv2"]])
-            print(DF)
-            print(DF_rings_dist.mean(axis=0))
-            DF = Analyse_generation.get_non_wass_stats_for_test()
-            print(DF)
+    plot_properties(args)
