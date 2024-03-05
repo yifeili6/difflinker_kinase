e05b76bd408ca60c5507c88973b4ae5bf6d08a43
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-04 23:49:21 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -127,7 +127,7 @@ def remove_one_atom_qed(mol: Chem.Mol, query_num_atoms: int) -> List[Chem.Mol]:
         try:
             qed = QED(eligand.GetMol())
         except:
-            qed = original_qed
+            qed = 0
         qed_diff = qed - original_qed
         contribs.append(qed_diff)
     contribs = [0] * query_num_atoms + contribs
