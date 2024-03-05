e2d9e13a1fc62e0ec552925b803f733f4f7ad6cb
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-04 22:38:45 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -186,6 +186,7 @@ def img_for_mol(mol: Chem.Mol, qry: Chem.Mol, query_num_atoms: int=None, atom_we
         atom_weights = SimilarityMaps.GetAtomicWeightsForFingerprint(qry, mol, SimilarityMaps.GetMorganFingerprint)
         atom_weights = np.array(atom_weights)
         atom_weights[:query_num_atoms] = 0
+        print(atom_weights)
         img = SimilarityMaps.GetSimilarityMapFromWeights(mol, atom_weights, draw2d=drawer) #http://rdkit.blogspot.com/2020/01/similarity-maps-with-new-drawing-code.html#:~:text=SimilarityMaps.GetSimilarityMapFromWeights(atorvastatin%2Clist(mean_chgs)%2Cdraw2d%3Dd)
         # print(img.savefig)
         with tempfile.TemporaryDirectory() as fp:
