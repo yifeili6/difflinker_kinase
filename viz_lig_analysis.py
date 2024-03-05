fb20ae46e5809930e7027677edef34387bd2f0a4
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-04 21:30:24 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -209,10 +209,12 @@ if __name__ == "__main__":
     root_d = "datasets"
     test_ms = [Chem.SDMolSupplier(os.path.join(root_h, f"5lqf_altB_chainA_3_{num}_KLIF_ValTest_frag.sdf"), removeHs=True, sanitize=True)[0] for num in [25, 27, 55, 60, 81, 82] ]
     test_ms = [Chem.RemoveHs(m) for m in test_ms]
-
+    query = Chem.SDMolSupplier(os.path.join(root, f"5lqf_altB_chainA_3_GT_KLIF_ValTest_frag.sdf"), removeHs=True, sanitize=False)[0]
+    query = Chem.RemoveHs(query)
+    
+    plot_similarity_maps(test_ms, query)
     
     # test_ms = [edit_ligand(m) for m in test_ms]
-    # query = Chem.SDMolSupplier(os.path.join(root, f"5lqf_altB_chainA_3_GT_KLIF_ValTest_frag.sdf"), removeHs=True, sanitize=False)[0]
     # qry = Chem.SDMolSupplier(os.path.join(root_d, f"KLIF_test_frag.sdf"), removeHs=True, sanitize=True)[900]
     # print(Chem.MolToSmiles(qry).split("."))
     # qry = [Chem.MolFromSmarts(q) for q in Chem.MolToSmiles(qry).split(".")]
