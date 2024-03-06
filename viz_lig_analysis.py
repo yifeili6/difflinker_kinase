807fd536235ba69cde00b59358b46e0ae835b84a
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-06 15:33:01 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -354,8 +354,8 @@ def plot_by_group(df: pd.DataFrame):
         index+=1
     handles, labels = ax[row_num][col_num].get_legend_handles_labels()
     
-    fig.tight_layout()
     fig.suptitle("5LQF AltB ChainA", y=1.2, fontsize=15)
+    fig.tight_layout()
     lgd = fig.legend(handles, labels, loc='lower center', ncol=9, bbox_to_anchor=(0.5, -0.05), framealpha=0, edgecolor='gray')
     fig.savefig(f'./test_.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
     
