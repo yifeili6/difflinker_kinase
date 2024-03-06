7bacf5aae81dc2706d5aa753db658dc64a40108b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-06 14:50:23 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -342,7 +342,7 @@ def plot_by_group(df: pd.DataFrame):
                 # sns.kdeplot(data=data.loc[:, metric].values.reshape(-1, ), label=n_atoms, ax=ax[row_num][col_num])
                 # Draw a nested boxplot to show bills by day and time
                 sns.boxplot(x="size", y=metric,
-                            data=data, ax=ax[row_num][col_num], palette="Set3")
+                            data=data, ax=ax[row_num][col_num], palette=sns.color_palette("pastel6", 6).as_hex())
         ax[row_num][col_num].set_xlabel(metric)
         ax[row_num][col_num].set_ylabel('Count')
         ax[row_num][col_num].set_title(metric + " distribution")
