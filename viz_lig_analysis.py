fc13b94e5abd18908599882c73fd5f164111b761
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-06 17:45:30 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -348,7 +348,7 @@ def plot_by_group(df: pd.DataFrame):
                 # Draw a nested boxplot to show bills by day and time
                 sns.violinplot(x="size", y=metric,
                             data=data, ax=ax[row_num][col_num], palette=[palette[idx]], label=n_atoms)
-        ax[row_num][col_num].set_xlabel(metric)
+        # ax[row_num][col_num].set_xlabel(metric)
         ax[row_num][col_num].set_ylabel('Count')
         ax[row_num][col_num].set_title(f"{metric} distribution", weight='bold', fontsize=13.5)  
         index+=1
