d505b12abeb6acad9f7d5273c84aae7555c05027
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-05 18:36:40 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -155,7 +155,7 @@ def remove_one_atom_sa(mol: Chem.Mol, query_num_atoms: int) -> List[Chem.Mol]:
         except:
             qed = 10
         qed_diff = qed - original_qed
-        print(qed)
+        print(qed_diff)
         contribs.append(qed_diff)
     contribs = [0] * query_num_atoms + contribs
     
