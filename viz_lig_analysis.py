bf4c77b485c5a61fdc800927f3742d5dbba0234b
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-06 15:17:53 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -339,14 +339,14 @@ def plot_by_group(df: pd.DataFrame):
                 ax[row_num][col_num].spines[['left','right', 'bottom']].set_visible(False)
                 # ax[row_num][col_num].hist(data.loc[:, metric].values.reshape(-1, ), **kwargs, label=n_atoms)
                 sns.barplot(x="size", y=metric,
-                            data=data, ax=ax[row_num][col_num], palette=palette[idx])
+                            data=data, ax=ax[row_num][col_num], palette=[palette[idx]])
             else:
                 data = df.loc[df.index[df.loc[:, "size"].apply(lambda inp: inp == n_atoms)]]
                 ax[row_num][col_num].spines[['left','right', 'bottom']].set_visible(False)
                 # sns.kdeplot(data=data.loc[:, metric].values.reshape(-1, ), label=n_atoms, ax=ax[row_num][col_num])
                 # Draw a nested boxplot to show bills by day and time
                 sns.violinplot(x="size", y=metric,
-                            data=data, ax=ax[row_num][col_num], palette=palette[idx])
+                            data=data, ax=ax[row_num][col_num], palette=[palette[idx]])
         ax[row_num][col_num].set_xlabel(metric)
         ax[row_num][col_num].set_ylabel('Count')
         ax[row_num][col_num].set_title(f"{metric} distribution", weight='bold', fontsize=13.5)  
