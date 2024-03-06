48b15bb08f0d4508743c73e49d7ebfdb9462b28e
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-06 15:21:23 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -353,9 +353,10 @@ def plot_by_group(df: pd.DataFrame):
         index+=1
     handles, labels = ax[row_num][col_num].get_legend_handles_labels()
     
-    plt.tight_layout()
+    fig.tight_layout()
     lgd = fig.legend(handles, labels, loc='lower center', ncol=9, bbox_to_anchor=(0.5, -0.05), framealpha=0, edgecolor='gray')
-    plt.savefig(f'./test_.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    fig.savefig(f'./test_.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    
     print(f'Plotted distribution of metrics')
     
 if __name__ == "__main__":
