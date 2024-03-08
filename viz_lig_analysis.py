baca5127ef607ab5c2f383b980f967d52b10bfde
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-08 14:01:34 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -368,6 +368,7 @@ if __name__ == "__main__":
     DF0, DF1 = merge_properties(args)
     print(DF0, DF1)
     print(DF1.columns)
+    DF0.drop(columns=["unique@10000","Scaf/Test","FCD/TestSF","SNN/TestSF","Frag/TestSF","Scaf/TestSF"], inplace=True)
     DF0.to_csv("latex_table.csv")
     plot_by_group(DF1)
     # plot_maps()
