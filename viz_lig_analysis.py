0108f28c2f85be84f98f516ff9cc98a865e44f6a
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2024-03-08 15:32:21 -0600

    Update viz_lig_analysis.py

--- viz_lig_analysis.py
+++ viz_lig_analysis.py
@@ -376,7 +376,7 @@ def plot_by_group(df: pd.DataFrame, args: argparse.ArgumentParser):
             ax[row_num][col_num].set_xlabel("No. of Sampled Atoms")
             ax[row_num][col_num].set_title(f"{metric} distribution", weight='bold', fontsize=9.5)  
             index+=1
-        axes[row_num, col_num].set_axis_off()
+        ax[row_num, col_num].set_axis_off()
 
     handles, labels = ax[row_num][col_num].get_legend_handles_labels()
     
